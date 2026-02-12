4ba4f3dd77fe815f2d87876526c2af1fd4c843f9
Harsh Guleria <guleriah59@gmail.com>
2026-02-12 22:09:24 +0530

    add js-refresher.js with initial code structure and comments

---
+++ t-class/js-refresher.js
@@ -0,0 +1,28 @@
+// Hello World
+console.log("Hello World!");
+
+// Code Structure
+// kebab-case file name. Use Semicolon
+
+// Topic: Comments
+// Interns should not touch this logic. [single line]
+/*
+  Line one
+  Line two
+  Line three
+[Multiline]
+ */
+
+// Topic: Variables
+// Steps --> create, store, use, modify
+
+let myFavLang;
+myFavLang = "JavaScript";
+console.log(myFavLang);
+
+myFavLang = "TypeScript";
+console.log("After learning TS my fav is: ", myFavLang);
+
+// Good Practice
+// Use camelCase for variable names
+
