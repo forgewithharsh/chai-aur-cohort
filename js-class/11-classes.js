5fc72fba45e0b5ad4acdbabc6f352a2103a7fda7
Harsh Guleria <guleriah59@gmail.com>
2026-02-22 21:51:59 +0530

    feat: add Cricketer class and Symbol examples

---
+++ js-class/11-classes.js
@@ -0,0 +1,9 @@
+class Cricketer {
+  constructor(name, role) {
+    this.name = name;
+    this.role = role;
+  }
+}
+
+const person1 = new Cricketer("Virat", "BatsMan")
+console.log(person1);
---
+++ js-class/12-Symbol.js
@@ -0,0 +1,7 @@
+const aadhaar_of_harsh = Symbol("aadhaar");
+const aadhaar_of_harry = Symbol("aadhaar");
+
+console.log(typeof aadhaar_of_harry);
+console.log(aadhaar_of_harry === aadhaar_of_harsh);
+console.log(aadhaar_of_harsh.toString());
+console.log(aadhaar_of_harry.description);
