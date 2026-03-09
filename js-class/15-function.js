a731b85c8fa59f1f12ba8d99e6931f342913cd5b
Harsh Guleria <guleriah59@gmail.com>
2026-03-10 02:11:24 +0530

    feat: add BMI calculator, discount calculation, and counter functions with console logging

--- js-class/15-function.js
+++ js-class/15-function.js
@@ -40,10 +40,39 @@ hello(function () {
 });
 
 // High Order Function
-function hod(){
-    return function() {
-      console.log("Hii Harsh")
-    }
+function hod() {
+  return function () {
+    console.log("Hii Harsh");
+  };
 }
 
-hod()()
+hod()();
+
+// BMI Calculator
+function bmi(weight, height) {
+  return weight / (height * height);
+}
+
+console.log(bmi(65, 1.8).toFixed(2));
+
+// Discount Calculate
+function discountCal(discount) {
+  return function (price) {
+    return price - price * (discount / 100);
+  };
+}
+
+let tenPercentDiscount = discountCal(10);
+console.log(tenPercentDiscount(1200));
+
+// Counter
+function counter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+let count = counter();
+console.log(count());
