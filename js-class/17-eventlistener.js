df656121039f75b9617619f81a74e6c8bb7b53d7
Harsh Guleria <guleriah59@gmail.com>
2026-03-19 22:28:50 +0530

    feat: update index.html and add event listener functionality

---
+++ js-class/17-eventlistener.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("button");
+
+// btn.onclick = function() {
+//   console.log("Hello world from dom");
+
+// }
+
+btn.addEventListener("click", function () {
+  console.log("Clicked");
+});
+
+const parent = document.getElementById("parent")
+const child = document.getElementById("child")
+
+
