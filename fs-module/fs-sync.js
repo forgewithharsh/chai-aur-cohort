de97e1a5f371aeb81e81f8001d1d7eb72e4f6ba5
Harsh Guleria <guleriah59@gmail.com>
2026-04-11 20:31:51 +0530

    feat: implement synchronous file operations in fs-sync.js

---
+++ fs-module/fs-sync.js
@@ -0,0 +1,19 @@
+import fs from "node:fs"
+
+// 1. Write
+
+fs.writeFileSync("test.txt", "Hello from sync fs!")
+
+// 2. Read
+const data = fs.readFileSync("test.txt", "utf-8")
+
+// console.log(data);
+
+// Append
+fs.appendFileSync("test.txt", "\nHello from append")
+
+// Mkdir
+fs.mkdirSync("myFolder/innerFolder", {recursive: true})
+
+// Unlink
+fs.unlinkSync("test.txt")
