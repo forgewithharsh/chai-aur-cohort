211097cc784b184a95be2df2946ef7f4cbe3d872
Harsh Guleria <guleriah59@gmail.com>
2026-04-28 20:00:28 +0530

    Add package.json for backend cohort with express dependency

--- backend-cohort/src/common/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  const connect = await mongoose.connect(process.env.MONGODB_URI);
-  console.log(`MongoDB connected: ${connect.connection.host}`);
-};
-
-export default connectDB;
---
+++ backend_cohort/01-express-intro.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+function block_1_basicServer() {
+  return new Promise((resolve) => {
+    const app = express();
+  });
+}
